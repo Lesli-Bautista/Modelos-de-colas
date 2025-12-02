2f995f913e43ec7c7db3c31d40719ad5ab8da35c
Lesli Bautista <lesliguadalupe34@gmail.com>
2025-12-01 22:11:08 -0600

    declaracion de variables

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int λ, μ;
+        double p, lq, wq,w;
+        byte opcion = 0;
+        
+    }
+
+}    
