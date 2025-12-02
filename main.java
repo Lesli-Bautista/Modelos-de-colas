e7ff9751ff94bb2372120431c1edecc69d579b0e
Lesli Bautista <lesliguadalupe34@gmail.com>
2025-12-01 22:36:20 -0600

    Modelo m/m/1

--- main.java
+++ main.java
@@ -12,9 +12,26 @@ public class Main {
             System.out.println("5. Salir");
             System.out.println("Opcion");
             opcion = sc.nextByte();
-            
+            swhitch (opcion) {
+                case 1 ->(opcion) {
+                    System.out.println( "Modelo m/m/1");
+                    System.out.print("Escribe el valor del llegadas λ: ");
+                    λ = sc.nextInt();
+                    System.out.print("Escribe el valor del Promedio servicio μ: ");
+                    μ = sc.nextInt();
+                    p= (double) λ / μ;
+                    lq =  (Math.pow(λ, 2)) / (μ*(μ - λ));
+                    wq = lq/λ;
+                    w= wq/μ
+                    System.out.println("Resultados:");
+                    System.out.println("Utilizacion del sistema p: " + p(p*100) + "%");
+                    System.out.println("Numero promedio de clientes en la cola Lq: " + lq);
+                    System.out.println("Tiempo promedio de espera en la cola Wq: " + wq + " horas o "+ (wq*60)+ " minutos");
+                    System.out.println("Tiempo promedio en el sistema W: " + w + " horas o "+ (w*60)+ " minutos");
+                }
+            }
 
-        }
+        }while (opción !=5);
         
     }
 
